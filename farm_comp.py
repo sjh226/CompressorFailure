ad163b582df474d5fc170b7a60869494fe66fc09
Steve <sharshman193@gmail.com>
2017-12-15 15:36:33 -0700

    running tests on farm data

--- farm_comp.py
+++ farm_comp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pyodbc
+import datetime
 
 
 def data_fetch():
@@ -23,9 +24,10 @@ def data_fetch():
 		SELECT CH.ID
 			  ,CH.Meter
 			  ,CH.Name
+			  ,CH.RecordDate
 			  ,CASE WHEN CH.RunStatus = 'Down' THEN 1
 					ELSE 0
-					END
+					END AS DownStatus
 			  ,CH.CompressorType
 			  ,CH.ConditionCode
 			  ,CH.GasVolumePDay
@@ -66,14 +68,36 @@ def data_fetch():
 
 	try:
 		df.columns = pd.DataFrame(np.matrix(cursor.description))[0]
-
+		df.columns = [col.lower().replace(' ', '_') for col in df.columns]
 	except:
 		pass
-        
+
 	connection.close()
+
+	return df
+
+def analyze(df):
+	for meter in df['meter'].unique():
+		unq = df[df['meter'] == meter]['recorddate'].value_counts()
+		print(meter, ' ', unq[-1])
+
+def model(df):
+	def fail_in(row):
+		days = 3
+		if np.mean(df[(df['recorddate'] > row['recorddate']) & \
+					  (df['recorddate'] <= (row['recorddate'] + \
+					  datetime.timedelta(days=days))) & \
+					  (df['meter'] == row['meter'])]['downstatus']) > 0:
+			return 1
+		else:
+			return 0
+
+	df['fail_in_week'] = df.apply(fail_in, axis=1)
 
 	return df
 
 
 if __name__ == '__main__':
 	df = data_fetch()
+	# analyze(df)
+	df = model(df)
